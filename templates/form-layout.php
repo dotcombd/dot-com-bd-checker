bf7dfb63ec0e2a51585e2922ebe6b2e244aadb53
dotcombd <sobujsfs@gmail.com>
2025-07-27 09:41:12 +0600

    Create form-layout.php

---
+++ templates/form-layout.php
@@ -0,0 +1,8 @@
+<div class="bdc-wrapper">
+  <h2 class="bdc-title"><?php echo esc_html(get_option('bdc_adv_welcome'));?></h2>
+  <div class="bdc-form">
+    <input type="text" id="bdc-domain-input" placeholder="Enter domain name (without extension)">
+    <button id="bdc-domain-submit">Search</button>
+  </div>
+  <div id="bdc-domain-result"></div>
+</div>
