8b3e575f8b507fd64686814a551d5303ddfd75d5
dotcombd <sobujsfs@gmail.com>
2025-07-27 10:01:31 +0600

    Update form-layout.php

--- templates/form-layout.php
+++ templates/form-layout.php
@@ -1,8 +1,10 @@
 <div class="bdc-wrapper">
-  <h2 class="bdc-title"><?php echo esc_html(get_option('bdc_adv_welcome'));?></h2>
-  <div class="bdc-form">
-    <input type="text" id="bdc-domain-input" placeholder="Enter domain name (without extension)">
-    <button id="bdc-domain-submit">Search</button>
-  </div>
-  <div id="bdc-domain-result"></div>
+    <h2 class="bdc-title">BD Domain Availability Checker</h2>
+
+    <div class="bdc-form">
+        <input type="text" id="bd-domain-input" placeholder="Enter domain name (without extension)">
+        <button id="bd-domain-submit">Search</button>
+    </div>
+
+    <div id="bd-domain-result"></div>
 </div>
